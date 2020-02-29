dbdd371919bdba3386f3dc617aab6e63983edb87
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-02-29 19:07:41 -0300

    Adiconando a espaçonave

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,18 @@
 import sys
 import pygame
 
-# Inicia o jogo e cria um objeto para a tela
+from settings import Settings
+
+# Inicializa o jogo, as configurações e o objeto screen
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)) 
     pygame.display.set_caption("Alien Invasion")
+    
+    # Define a cor do fundo
+    bg_color = (230,230,230)
     
     # Inicia o laço principal do jogo
     while True:
@@ -14,6 +21,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(ai_settings.bg_color)
         
         # Deixa a tela mais recente visível
         pygame.display.flip()
