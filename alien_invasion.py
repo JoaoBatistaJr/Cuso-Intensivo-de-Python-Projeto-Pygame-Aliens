d9b68a79e8e5718394582bef94a1d670051d5ff0
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-03-05 16:47:50 -0300

    Melhorando o codigo do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,34 +1,21 @@
-import pygame
+import sys
 
-from pygame.sprite import Group
+import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
-import game_functions as gf
+class AlienInvasion:
+# Classe principal que gerencia todo o comportamento do jogo.
 
-# Inicializa o jogo, as configurações e o objeto screen
-def run_game():
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height)) 
-    pygame.display.set_caption("Alien Invasion")
-    
-    # Cria uma espaçonave
-    ship = Ship(ai_settings, screen)
-    
-    #Cria um grupo no qual serão armazenados os projéteis.
-    bullets = Group()
-    
-    # Define a cor do fundo
-    bg_color = (230,230,230)
-    
-    # Inicia o laço principal do jogo
-    while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
-        
-run_game()
+def __init__(self):
+  """Inicializa e cria os recursos do jogo."""
+  pygame.init()
+  self.settings = Settings()
+  
+  self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+  self.settings.screen_width = self.screen.get_rect().width
+  self.settings.screen_height = self.screen.get_rect().height
+  pygame.display.set_caption("Alien Invasion")
