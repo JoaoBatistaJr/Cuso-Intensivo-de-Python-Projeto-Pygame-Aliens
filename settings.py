60827edb022fb89c3105a52ee79a8968256494a8
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-03-06 12:52:12 -0300

    Alterações da classe Settings

--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
-class Settings():
-    """Uma classe para armazenar todas as configurações da Invasão Alienígena."""
-    
+class Settings:
+    """Classa que armazena todas as configurações do jogo"""
+
     def __init__(self):
         """Inicializa as configurações do jogo."""
-        
-        #Configurações da tela
-        self.screen_width = 800
-        self.screen_height = 600
-        self.bg_color = (230,230,230)
-        
-        #Configurações da espaçonave
-        self.ship_speed_factor = 1.5
-        
-        #Configuração de projéteis. 
-        self.bullet_speed_factor = 1
+        # Configuraçoes da tela.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Configurações da Nave.
+        self.ship_speed = 1.5
+
+        # Configuração dos projéteis.
+        self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
