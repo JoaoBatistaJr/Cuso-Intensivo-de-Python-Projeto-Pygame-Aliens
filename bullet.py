4b9240a80bcb8bba85f3c6371ee412c1d63f5601
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2020-03-06 14:01:49 -0300

    ajustes na classe bullet

--- bullet.py
+++ bullet.py
@@ -5,28 +5,26 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """Uma classe que administra projéteis disparados pela espaçonave"""
     
-    def __init__(self, ai_settings, screen, ship):
+    def __init__(self, ai_game):
         """Cria um objeto para o projétil na posição atual da espaçonave."""
-        super(Bullet, self).__init__()
-        self.screen = screen
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
         
         # Cria um retângulo para o projétil em (0, 0) e, em seguida, define
         # a posição correta
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, 
-                                ai_settings.bullet_height)
-        self.rect.centerx = ship.rect.centerx
-        self.rect.top = ship.rect.top
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
         
         # Armazena a posição do projétil como um valor decimal
         self.y = float(self.rect.y)
         
-        self.color = ai_settings.bullet_color
-        self.speed_factor = ai_settings.bullet_speed_factor
-        
     def update(self):
         """Move o projétil para cima na tela."""
         # Atualiza a posição decimal do projétil
-        self.y -= self.speed_factor
+        self.y -= self.settings.bullet_speed
         # Atualiza a posição de rect
         self.rect.y = self.y
         
